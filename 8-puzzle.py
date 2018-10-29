7bfa4fdb9132489649c3f4ad2598b5feefae07c9
acertainelijah <emarc004@ucr.edu>
2018-10-29 11:50:09 -0700

    Added the template algorithm choosing

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -46,6 +46,14 @@ def main():
     print("Your init state of current puzzle:" + '\n')
     print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in curr_puzzle]))
 
+    curr_algorithm = input("Enter your choice of algorithm: " + '\n'
+                        + "    (1: Uniform Cost)" + '\n'
+                        + "    (2: A* with the Misplaced Tile heuristic)" + '\n'
+                        + "    (3: A* with the Manhattan distance heuristic)" + '\n')
+
+    #run puzzle on a selected A* search algorithm
+    run_algorithm(curr_puzzle, curr_algorithm)
+
 
 
 def choose_default_puzzle():
@@ -57,11 +65,24 @@ def create_puzzle():
     temp_puzzle = []
     print("Enter your 3x3 puzzle one row at a time, with each col separated by a space")
     for i in xrange(3):
-        temp_puzzle.append((raw_input('Input for row ' + str(i) + ': ').split()))
+        temp_puzzle.append(list(int(i) for i in (raw_input('Input for row ' + str(i) + ': ').split())))
+    #temp_puzzle = [(int(i) for i in row) & list(row) for row in temp_puzzle]
+    #temp_puzzle = [int(i) for i in temp_puzzle]
+    print type(temp_puzzle)
+    print type(temp_puzzle[0])
+    print type(temp_puzzle[0][0])
     print temp_puzzle
     return temp_puzzle
 
-#def choose_algorithm(puzzle):
+def run_algorithm(puzzle, algorithm_number):
+    print('\n')
+    if algorithm_number == 1:
+        print("You selected: Uniform Cost" + '\n')
+    if algorithm_number == 2:
+        print("You selected: A* with the Misplaced Tile heuristic" + '\n')
+    if algorithm_number == 3:
+        print("You selected: Manhattan distance heuristic" + '\n')
+
 
 #below 2 lines are used to call main function
 if __name__ == "__main__":
