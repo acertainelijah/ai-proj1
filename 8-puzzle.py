f01a085c9a884099b133427240c76b4336df21bb
acertainelijah <emarc004@ucr.edu>
2018-10-29 12:57:39 -0700

    updating  uniform cost search

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -24,10 +24,18 @@ impossible = [[1, 2, 3],
               [4, 5, 6],
               [8, 7, 0]]
 
+default_puzzles = [trivial, very_easy, easy, doable, oh_boy, impossible]
+
 eight_goal_state = [[1, 2, 3],
                     [4, 5, 6],
                     [7, 8, 0]]
-default_puzzles = [trivial, very_easy, easy, doable, oh_boy, impossible]
+
+#update and return these 3 numbers at the end of the program
+total_nodes = 0
+max_in_queue = 0
+goal_depth = 0
+
+
 
 def main():
     puzzle_mode = input("Welcome to an 8-Puzzle Solver." + '\n'
@@ -39,12 +47,12 @@ def main():
         curr_puzzle = choose_default_puzzle()
         #choose_algorithm(choose_default_puzzle()
     if puzzle_mode == 2:
-        print("Enter your own puzzle!")
+        print("Enter your own puzzle, use a zero to represent the blank")
         curr_puzzle = create_puzzle()
 
     # Print current puzzle
     print("Your init state of current puzzle:" + '\n')
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in curr_puzzle]))
+    print_puzzle(curr_puzzle)
 
     curr_algorithm = input("Enter your choice of algorithm: " + '\n'
                         + "    (1: Uniform Cost)" + '\n'
@@ -54,16 +62,17 @@ def main():
     #run puzzle on a selected A* search algorithm
     run_algorithm(curr_puzzle, curr_algorithm)
 
-
+def print_puzzle(puzzle):
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in puzzle]))
 
 def choose_default_puzzle():
     selected_difficulty = input("You choose to use a default puzzle. Please enter a desired difficulty on a scale from "
-                                "0 to 5." + '\n')
+                                "0 to 5 (with 5 being impossible!)." + '\n')
     return default_puzzles[selected_difficulty]
 
 def create_puzzle():
     temp_puzzle = []
-    print("Enter your 3x3 puzzle one row at a time, with each col separated by a space")
+    print("Enter your 3x3 puzzle, one row at a time, with each column separated by a space")
     for i in xrange(3):
         temp_puzzle.append(list(int(i) for i in (raw_input('Input for row ' + str(i) + ': ').split())))
     #temp_puzzle = [(int(i) for i in row) & list(row) for row in temp_puzzle]
@@ -74,14 +83,41 @@ def create_puzzle():
     print temp_puzzle
     return temp_puzzle
 
+
 def run_algorithm(puzzle, algorithm_number):
     print('\n')
     if algorithm_number == 1:
         print("You selected: Uniform Cost" + '\n')
+        uniform_cost_search(puzzle)
     if algorithm_number == 2:
         print("You selected: A* with the Misplaced Tile heuristic" + '\n')
     if algorithm_number == 3:
         print("You selected: Manhattan distance heuristic" + '\n')
+
+def uniform_cost_search(puzzle):
+    print("Expanding state: ")
+    print_puzzle(puzzle)
+    init_node = Node(1,0,0, puzzle)
+    run_A_star(init_node)
+
+def run_A_star(start_node):
+    #how do I know what children to add?
+    #how do I take this 8-puzzle and put it into a node data structure
+    start_node.add_child()
+
+
+
+class Node(object):
+    def __init__(self, g, h, f, puzzle):
+        self.g = g  # cost to next node
+        self.h = h  # heuristic value from goal state
+        self.f = f  # f = g + h
+        self.puzzle = puzzle
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
 
 
 #below 2 lines are used to call main function
